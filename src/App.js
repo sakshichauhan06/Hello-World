2c6401d3828ceca8b345446b6c2cffee83d44840
Sakshi <chauhansakshi4m@gmail.com>
2023-02-03 01:27:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import HelloWorld from "./HelloWorld";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // In the below code, I've created a separated component "HelloWorld.js", by doing this I would be able to edit the content & styling of this component quite easily. By creating components, it becomes easier to handle a lot of pages in our website.
+      <div className="container">
+        <HelloWorld />
+      </div>
+
+      // Below code represents a simple way of creating Hello World by just adding the JSX into our code.
+      // <div className="container">A simple Hello World</div>
+    )
+  }
 }
 
-export default App;
+export default App
